59a84bceb6360b69450d482ef7c1cd655aae3480
Javier Brito <vjbritoa@hotmail.com>
2023-10-03 16:03:55 -0500

    Ajustes en generar transacción, adicionar envio de correo

--- ASEDINFO_FRONTEND/src/environments/environment.ts
+++ ASEDINFO_FRONTEND/src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
   hmr: false,
   prefijoApp:"SEG",
   //url_seguridad: 'http://localhost:8080/seguridad-servicios',
-  url_seguridad: 'http://localhost:8080/asedinfo-servicios',
-  //url_seguridad: 'https://spring.asedinfo.com/asedinfo-servicios',
+  //url_seguridad: 'http://localhost:8080/asedinfo-servicios',
+  url_seguridad: 'https://spring.asedinfo.com/asedinfo-servicios',
   //url_catalogo: 'http://localhost:8080/catalogo-servicios',
 };
 
